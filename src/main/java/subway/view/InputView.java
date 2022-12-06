4ccf7615fbfa751bfeeba5c60c83f426837f3845
jinny-l <jinny.l.dev@gmail.com>
2022-12-06 17:19:06 +0900

    feat: 등록/삭제할 역 이름 입력 기능 구현
    * InputView.java
    등록/삭제할 역 이름 입력 기능 구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,14 +1,13 @@
 package subway.view;
 
 import java.util.Scanner;
-import subway.constant.MainCommand;
 
 public class InputView {
 
     private static final String INPUT_VIEW_PREFIX = "## ";
     private static final String INPUT_MAIN_COMMAND = INPUT_VIEW_PREFIX + "원하는 기능을 선택하세요.";
-    private static final String INPUT_STATION_TO_REGISTER = "등록할 역 이름을 입력하세요.";
-    private static final String INPUT_STATION_TO_DELETE = "삭제할 역 이름을 입력하세요.";
+    private static final String INPUT_STATION_TO_REGISTER = INPUT_VIEW_PREFIX + "등록할 역 이름을 입력하세요.";
+    private static final String INPUT_STATION_TO_DELETE = INPUT_VIEW_PREFIX + "삭제할 역 이름을 입력하세요.";
     private static final String INPUT_LINE_TO_REGISTER = "등록할 노선 이름을 입력하세요.";
     private static final String INPUT_LINE_TO_DELETE = "삭제할 노선 이름을 입력하세요.";
 //    private static final String = "등록할 노선의 상행 종점역 이름을 입력하세요.";
@@ -27,9 +26,19 @@ public class InputView {
 
     public String readCommand() {
         System.out.println(INPUT_MAIN_COMMAND);
-        return MainCommand.from(readLine());
+        return readLine();
     }
 
+    public String readStationToRegister() {
+        System.out.println(INPUT_STATION_TO_REGISTER);
+        return readLine();
+    }
+
+    public String readStationToDelete() {
+        System.out.println(INPUT_STATION_TO_DELETE);
+        return readLine();
+    }
+    
     private String readLine() {
         String input = scanner.nextLine().trim();
         validateHasInput(input);
