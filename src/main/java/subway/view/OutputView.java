349aa75b06c35b5c837f6ef348bfbbcdaa676a14
jinny-l <jinny.l.dev@gmail.com>
2022-12-06 17:44:42 +0900

    fix: 역 목록 출력 기능 구현
    * OutputView.java
    역 목록 출력 기능 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,13 +1,21 @@
 package subway.view;
 
-import subway.constant.MainCommand;
-import subway.constant.StationManagementCommand;
+import subway.constant.command.MainCommand;
+import subway.constant.command.StationManagementCommand;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 
 public class OutputView {
 
     private static final String OUTPUT_VIEW_PREFIX = "## ";
+    private static final String INFORMATION_PREFIX = "[INFO] ";
     private static final String MAIN = OUTPUT_VIEW_PREFIX + "메인 화면%n%s%n";
     private static final String STATION_MANAGEMENT = OUTPUT_VIEW_PREFIX + "%s 화면%n%s%n";
+    private static final String STATION_LIST = OUTPUT_VIEW_PREFIX + "%s 화면%n%s%n";
+
+    public static void printError(String message) {
+        System.out.println(message);
+    }
 
     public static void printMain() {
         StringBuilder commandView = new StringBuilder();
@@ -25,5 +33,11 @@ public class OutputView {
         System.out.printf(STATION_MANAGEMENT, MainCommand.STATION_MANAGEMENT.getText(), StationManagementCommandView);
     }
 
+    public static void printStations() {
+        StationRepository.stations().stream()
+                .map(Station::getName)
+                .forEach(name -> System.out.println(INFORMATION_PREFIX + name));
+        System.out.println();
+    }
 
 }
