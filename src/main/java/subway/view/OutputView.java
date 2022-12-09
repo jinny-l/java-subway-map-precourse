2e6d7f54504f4da8a9db03fbb23e28e1f342c7c2
jinny-l <jinny.l.dev@gmail.com>
2022-12-09 15:28:53 +0900

    feat: 역/노선/구간의 등록/삭제 여부 출력 기능 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -7,6 +7,7 @@ import subway.constant.command.StationCommand;
 public class OutputView {
 
     private static final String OUTPUT_VIEW_PREFIX = "## ";
+    private static final String INFORMATION_PREFIX = "[INFO] ";
 
     public static void printMainCommand() {
         StringBuilder stringBuilder = new StringBuilder();
@@ -20,6 +21,14 @@ public class OutputView {
         Stream.of(StationCommand.values())
                 .forEach(stationCommand -> stringBuilder.append(stationCommand.toString()));
         System.out.printf("%s역 관리 화면%n%s%n", OUTPUT_VIEW_PREFIX, stringBuilder);
+    }
+
+    public static void printRegisteredInfo(String object) {
+        System.out.printf("%s%s이 등록되었습니다.%n", INFORMATION_PREFIX, object);
+    }
+
+    public static void printDeletedInfo(String object) {
+        System.out.printf("%s%s이 삭제되었습니다.%n", INFORMATION_PREFIX, object);
     }
 
 }
