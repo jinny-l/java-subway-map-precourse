8c6a0535996984b816c2addeb567b0153c2cf0d1
jinny-l <jinny.l.dev@gmail.com>
2022-12-09 15:46:40 +0900

    feat: 에러 메시지 출력 기능 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -11,6 +11,10 @@ public class OutputView {
     private static final String OUTPUT_VIEW_PREFIX = "## ";
     private static final String INFORMATION_PREFIX = "[INFO] ";
 
+    public static void printError(String message) {
+        System.out.println(message);
+    }
+
     public static void printMainCommand() {
         StringBuilder stringBuilder = new StringBuilder();
         Stream.of(MainCommand.values())
@@ -22,18 +26,19 @@ public class OutputView {
         StringBuilder stringBuilder = new StringBuilder();
         Stream.of(StationCommand.values())
                 .forEach(stationCommand -> stringBuilder.append(stationCommand.toString()));
-        System.out.printf("%s역 관리 화면%n%s%n", OUTPUT_VIEW_PREFIX, stringBuilder);
+        System.out.printf("%n%s역 관리 화면%n%s%n", OUTPUT_VIEW_PREFIX, stringBuilder);
     }
 
     public static void printRegisteredInfo(String object) {
-        System.out.printf("%s%s이 등록되었습니다.%n", INFORMATION_PREFIX, object);
+        System.out.printf("%n%s지하철 %s이 등록되었습니다.%n%n", INFORMATION_PREFIX, object);
     }
 
     public static void printDeletedInfo(String object) {
-        System.out.printf("%s%s이 삭제되었습니다.%n", INFORMATION_PREFIX, object);
+        System.out.printf("%n%s지하철 %s이 삭제되었습니다.%n%n", INFORMATION_PREFIX, object);
     }
 
     public static void printStations() {
+        System.out.printf("%n%s 역 목록%n", OUTPUT_VIEW_PREFIX);
         StationRepository.stations().stream()
                 .map(Station::getName)
                 .forEach(name -> System.out.println(INFORMATION_PREFIX + name));
